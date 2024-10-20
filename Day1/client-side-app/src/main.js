49bc2931a3e818501f9e7c6a357e0318390e4ca5
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-10-20 15:33:00 +0530

    Adding main file

---
+++ Day1/client-side-app/src/main.js
@@ -0,0 +1,13 @@
+// Imports the file and run's the global code in the file, without importing anything
+// import './1_datatypes/1_declarations';
+// import './1_datatypes/2_es6_declarations';
+// import './1_datatypes/3_datatypes';
+// import './1_datatypes/4_template_literals';
+
+// import './2_functions/1_fn_creation';
+// import './2_functions/2_fn_parameters';
+// import './2_functions/3_rest_and_spread';
+// import './2_functions/4_pure_impure_fn';
+// import './2_functions/5_hof';
+import './2_functions/6_fn_context';
+
